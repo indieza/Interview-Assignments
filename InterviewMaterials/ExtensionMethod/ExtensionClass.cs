b2755731f3c86649fc04771c951d069004db8327
indieza <simeonvalev00@gmail.com>
2023-01-16 16:57:31 +0200

    Implement base structure

---
+++ InterviewMaterials/ExtensionMethod/ExtensionClass.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtensionMethod
+{
+    public static class ExtensionClass
+    {
+        public static int ExtensionMethod(this SealedClass item)
+        {
+            return item.Sum();
+        }
+    }
+}
---
+++ InterviewMaterials/ExtensionMethod/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionMethod
+{
+    public class StartUp
+    {
+        private static void Main()
+        {
+            var result = new SealedClass(1, 3).Sum();
+            Console.WriteLine(result);
+        }
+    }
+}
